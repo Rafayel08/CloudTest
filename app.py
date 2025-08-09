631fa707d4bd0c1238a77c9c1b7f27b47acf692c
Rafayel S <85800775+Rafayel08@users.noreply.github.com>
2025-08-09 20:51:10 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
+from streamlit_autorefresh import st_autorefresh
+
+st_autorefresh(interval=60 * 1000, key="auto_refresh")
 
 stock = yf.Ticker("AAPL")
 
